b733c049bc95e31106ce7124e560c8a44084d5c2
im-yukii <yuukiimnida@gmail.com>
2026-03-11 06:29:57 +0900

    initial upload

---
+++ demo_site01/js/slick_ctrl.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(function(){
+    $(function(){
+        $('#slider01').slick({
+            fade:true,
+            speed:1500,
+            arrows:true,
+            prevArrow: '<button class="slide_arrow prev_arrow"></button>',
+            nextArrow: '<button class="slide_arrow next_arrow"></button>',
+             });
+         });
+    });
